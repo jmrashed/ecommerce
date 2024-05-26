8b11efd6be5fb941a013b9123d12b48fd5d915cb
RASHED <jmrashed@gmail.com>
2024-05-26 17:36:03 +0600

    update

--- database/migrations/2024_05_25_000107_create_pkg_payments_table.php
+++ database/migrations/2024_05_25_000107_create_pkg_payments_table.php
@@ -4,7 +4,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePkgPaymentsTable extends Migration
+
+return new class extends Migration
 {
     /**
      * Run the migrations.
